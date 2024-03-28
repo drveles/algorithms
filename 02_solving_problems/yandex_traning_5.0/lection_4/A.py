57f3ee35ef035a9da9ff1dac437dde70bd9c60a5
Jenniffer Ruben <frolov.vasilij.2014@yandex.ru>
2024-03-28 22:17:08 +0300

    quick save

--- 02_solving_problems/yandex_traning_5.0/lection_4/A.py
+++ 02_solving_problems/yandex_traning_5.0/lection_4/A.py
@@ -1,17 +1,22 @@
-_ = input()
-arr = tuple(map(int, input().split()))
-k = int(input())
-count_dict = {}
+def main():
+    _ = int(input())
+    arr = list(map(int, input().split()))
+    k = int(input())
+    count_dict = {}
 
-for num in arr:
-    count_dict[num] = count_dict.get(num, 0) + 1
+    for num in arr:
+        count_dict[num] = count_dict.get(num, 0) + 1
 
-for _ in range(k):
-    l, r = map(int, input().split())
-    count = 0
-    for num in count_dict:
-        if l <= num <= r:
-            count += count_dict[num]
-    print(count, sep='', end=' ')
+    for i in range(k):
+        l, r = map(int, input().split())
+        count = 0
+        for num in count_dict:
+            if l <= num <= r:
+                count += count_dict[num]
+        print(count, sep='', end=' ')
 
-# TL
+
+if __name__ == "__main__":
+    main()
+
+#faster variant
