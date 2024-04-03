924375b3cb270340ffecdaaf81fd4f6b60d90f6b
Jenniffer Ruben <frolov.vasilij.2014@yandex.ru>
2024-04-03 20:08:06 +0300

    739 sovled

--- 02_solving_problems/01_leetcode/02_medium/m_0739_1.py
+++ 02_solving_problems/01_leetcode/02_medium/m_0739_1.py
@@ -1,14 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temperatures):
         mono_stack = []
-        
+
         result = [0 for _ in range(len(temperatures))]
-        
+
         for id, el in enumerate(temperatures):
             if not mono_stack:
                 mono_stack.append(id)
                 continue
-            while mono_stack and temperatures[mono_stack[-1]] < el: 
+            while mono_stack and temperatures[mono_stack[-1]] < el:
                 result[mono_stack[-1]] = id - mono_stack[-1]
                 mono_stack.pop()
             else:
