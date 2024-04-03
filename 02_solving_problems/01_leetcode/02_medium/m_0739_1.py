1bf7f53d03a8b31077e6b37432412a7b8abfa656
Jenniffer Ruben <frolov.vasilij.2014@yandex.ru>
2024-04-03 20:12:49 +0300

    739 sovled

--- 02_solving_problems/01_leetcode/02_medium/m_0739_1.py
+++ 02_solving_problems/01_leetcode/02_medium/m_0739_1.py
@@ -1,17 +1,21 @@
 class Solution:
-    def dailyTemperatures(self, temperatures):
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         mono_stack = []
-
-        result = [0 for _ in range(len(temperatures))]
-
-        for id, el in enumerate(temperatures):
+        t_len = len(temperatures)
+        result = [0 for _ in range(t_len)]
+        
+        for id in range(t_len):
             if not mono_stack:
                 mono_stack.append(id)
                 continue
-            while mono_stack and temperatures[mono_stack[-1]] < el:
+            while mono_stack and temperatures[mono_stack[-1]] < temperatures[id]: 
                 result[mono_stack[-1]] = id - mono_stack[-1]
                 mono_stack.pop()
             else:
                 mono_stack.append(id)
 
         return result
+
+
+# normal solution, using monotonic stack
+# 39% time, 59% mem
