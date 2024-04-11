a059ed59c288afc79804c9250ff66b676a20e1a4
Jenniffer Ruben <frolov.vasilij.2014@yandex.ru>
2024-04-11 15:25:25 +0300

    A solved

---
+++ 00_archive/trie.py
@@ -0,0 +1,45 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end_of_word = True
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end_of_word
+    
+    def starts_with(self, prefix):
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+
+# Пример использования
+trie = Trie()
+words = ["apple", "banana", "orange", "app"]
+for word in words:
+    trie.insert(word)
+
+print(trie.search("apple")) 
+print(trie.search("banana")) 
+print(trie.search("pear")) 
+print(trie.starts_with("aap")) 
+print(trie.starts_with("app")) 
+
