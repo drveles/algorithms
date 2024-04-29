66d95a26bf4a4005e515952f91667f6e8be2306b
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-04-29 13:51:43 +0300

    1493 solved

---
+++ 02_solving_problems/01_leetcode/02_medium/m_1493_1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestSubarray(self, nums) -> int:
+        res = last = curr = 0
+
+        for num in nums:
+            if num: 
+                curr += 1 
+            else:
+                last, curr = curr, 0
+            res = max(res, last + curr)
+
+        return res - (len(nums) == res)
+    
+if __name__ == "__main__":
+    # print(Solution().longestSubarray([0, 0, 0, 0]))
+    # print(Solution().longestSubarray([0, 1]))
+    print(Solution().longestSubarray([1,1,0,1]))
