b58767f105895599c3375818703135d3bef54869
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-06-01 13:31:29 +0300

    49 resolved

---
+++ 02_solving_problems/01_leetcode/02_medium/m_0049_2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams_dct = {}
+
+        for word in strs:
+            sort_word = str(sorted(word))
+            if sort_word in anagrams_dct:
+                anagrams_dct[sort_word].append(word)
+            else: anagrams_dct[sort_word] = [word]
+        
+        return [sorted(s) for s in (sorted(anagrams_dct.values(), key=lambda x: len(x)))]
+
+
+# OK 35%, 43%
