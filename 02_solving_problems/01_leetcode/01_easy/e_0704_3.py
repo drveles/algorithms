95e981d64c364dc3ebd6438896390dc1ab37a37f
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-09-06 18:08:57 +0300

    704 resolved

--- 02_solving_problems/01_leetcode/01_easy/e_0704_3.py
+++ 02_solving_problems/01_leetcode/01_easy/e_0704_3.py
@@ -1,8 +1,8 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        idx = bisect_left(nums, target) 
-        
+        idx = bisect_left(nums, target)
+
         return idx if idx < len(nums) and nums[idx] == target else -1
 
 
-#OK, 20%, 28%
+# OK, 20%, 28%
