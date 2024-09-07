90f1ec3afc9dd4191ad13f8bb4149a7e6b731fb8
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-09-07 13:29:55 +0300

    986 solved

---
+++ 02_solving_problems/01_leetcode/02_medium/m_0986_1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        result = []
+
+        if not firstList or not secondList:
+            return result
+
+        first = 0
+        second = 0
+
+        while first < len(firstList) and second < len(secondList):
+            if firstList[first][0] > secondList[second][1]:
+                second += 1
+            elif secondList[second][0] > firstList[first][1]:
+                first += 1
+            else:
+                result.append(
+                    [
+                        max(firstList[first][0], secondList[second][0]),
+                        min(firstList[first][1], secondList[second][1]),
+                    ]
+                )
+                if firstList[first][1] < secondList[second][1]:
+                    first += 1
+                else:
+                    second += 1
+
+        return result
+
+
+# OK, 93%, 68%
