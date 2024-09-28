718749d9ec11ad0a9e2558f93e281b14fee2d7f2
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-09-28 20:17:48 +0300

    140 biweekly contest solved 2/4

--- 02_solving_problems/01_leetcode/04_contests/0140_biweekly/q1.py
+++ 02_solving_problems/01_leetcode/04_contests/0140_biweekly/q1.py
@@ -1,5 +1,6 @@
 class Solution:
     def minElement(self, nums: List[int]) -> int:
         return min(sum(map(int, list(str(num)))) for num in nums)
-    
+
+
 # OK
--- 02_solving_problems/01_leetcode/04_contests/0140_biweekly/q2.py
+++ 02_solving_problems/01_leetcode/04_contests/0140_biweekly/q2.py
@@ -3,18 +3,19 @@ class Solution:
         towers = sorted(maximumHeight)
         prev = towers[-1] + 1
         summ = 0
-        
+
         for tower in towers[::-1]:
             temp = tower
 
             while temp >= prev:
                 temp = prev - 1
-            
+
             if temp < 1:
                 return -1
             summ += temp
             prev = temp
-        
+
         return summ
-    
-#OK
+
+
+# OK
