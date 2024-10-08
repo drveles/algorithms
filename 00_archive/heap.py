40b378078d19b63e0600a22077ef00cf02517645
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-10-08 12:42:37 +0300

    touching heapq

--- 00_archive/heap.py
+++ 00_archive/heap.py
@@ -2,5 +2,10 @@ import heapq
 
 arr = [10,1,2,3,4]
 heapq.heapify(arr)
-heapq.hea
 print(type(arr))
+print(arr)
+print(heapq.heappop(arr))
+print(heapq.heappop(arr))
+print(heapq.heappop(arr))
+print(heapq.heappop(arr))
+print(heapq.heappop(arr))
