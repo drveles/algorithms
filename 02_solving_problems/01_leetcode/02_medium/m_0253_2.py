fe5cfc650d936589d7fef569c5a315a62b6334a5
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-10-08 12:57:28 +0300

    253 optimized

--- 02_solving_problems/01_leetcode/02_medium/m_0253_2.py
+++ 02_solving_problems/01_leetcode/02_medium/m_0253_2.py
@@ -13,9 +13,8 @@ class Solution():
         heapq.heappush(heap, intervals[0][1])
 
         for start, end in intervals[1:]:
-            prev_end = heapq.heappop(heap)
-            if start < prev_end:
-                heapq.heappush(heap, prev_end)
+            if start >= heap[0]:
+                heapq.heappop(heap)
             heapq.heappush(heap, end)
 
         return len(heap)
