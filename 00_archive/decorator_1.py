dc22e50b38b93916a882331008b7a9e9e469bd2b
jenniffr <frolov.vasilij.2014@yandex.ru>
2024-10-20 11:58:25 +0300

    another decorator

---
+++ 00_archive/decorator_1.py
@@ -0,0 +1,18 @@
+from functools import wraps
+
+
+def decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print("hello from decorator")
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@decorator
+def foo(int_arg: int, other_str_arg: str) -> str:
+    return f"hello from func with args {int_arg},{other_str_arg}"
+
+
+print(foo(1, other_str_arg="str"))
