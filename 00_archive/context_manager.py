8622e1ca48bfe41838f9c824ae6fffcb8e5ec102
Vasilii Frolov <frolov.vasilij.2014@yandex.ru>
2025-02-18 00:22:01 +0300

    upd: contex_manager.py

--- 00_archive/context_manager.py
+++ 00_archive/context_manager.py
@@ -1,8 +1,17 @@
 class CustomContextManager():
     _opened_file = None
 
-    def __enter__():
+    def __enter__(self):
         print('Entered')
         
-    def __exit__():
+    def __exit__(self, type, value, traceback):
         print('Exited')
+
+
+def main():
+    with CustomContextManager(): 
+        print('In context block')
+
+
+if __name__ == '__main__':
+    main()
