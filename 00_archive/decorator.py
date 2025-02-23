46aa1c72e5b957ec1ba073b16eb7e62198e2b22e
Vasilii Frolov <frolov.vasilij.2014@yandex.ru>
2025-02-24 00:05:27 +0300

    upd: decorator

--- 00_archive/decorator.py
+++ 00_archive/decorator.py
@@ -1,30 +1,24 @@
 from functools import wraps
 
-
-def decorator_without_params(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        print("Inside decorator_without_params")
-        return func(*args, **kwargs)
-    return wrapper
-
-
-def decorator_with_params(repeat_times: int):
-    def decorator(func):
+def decorator_with_params(parameter: str):
+    def main_wrapper(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            for _ in range(repeat_times):
-                print("Inside decorator_with_params")
-            return func(*args, **kwargs)
+            print(f'decorator {parameter=}')
+            print(f'{args=},{kwargs=}')
+            return func(args, kwargs)
         return wrapper
-    return decorator
+    return main_wrapper
 
 
-@decorator_with_params(3)
-@decorator_without_params
-def funciton(message: str = "Hello from function"):
-    print(message)
+@decorator_with_params('some parameter')
+def foo(kwarg):
+    print(f'Insade function with {kwarg=}')
 
 
-if __name__ == "__main__":
-    funciton()
+def main():
+    foo('arg', kwarg='kwarg')
+
+
+if __name__ == '__main__':
+    main()
