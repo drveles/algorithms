70cea191342ade9b5e9b77ec9b6f5220a49c33eb
DrVeles <frolov.vasilij.2014@yandex.ru>
2025-05-22 19:19:16 +0300

    add: 146 resolved

---
+++ 02_solving_problems/01_leetcode/02_medium/m_0146_3.py
@@ -0,0 +1,16 @@
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        self.cache[key] = value
+        self.cache.move_to_end(key)
+        if self.capacity < len(self.cache):
+            self.cache.popitem(last=False)
