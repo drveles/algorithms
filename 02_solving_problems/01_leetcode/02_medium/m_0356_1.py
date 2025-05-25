531c8410b2f6c52e1accb4d86bd14b392c028ca0
DrVeles <frolov.vasilij.2014@yandex.ru>
2025-05-25 21:53:50 +0300

    style: 356 formatted

--- 02_solving_problems/01_leetcode/02_medium/m_0356_1.py
+++ 02_solving_problems/01_leetcode/02_medium/m_0356_1.py
@@ -1,44 +1,9 @@
-# Даны координаты вида x1,y1 и x2, y2
-# определить, можно ли провести паралельную прямую,
-# которая бы делила расстояние между ними пополам?
+"""
+Даны координаты вида x1,y1 и x2, y2
+определить, можно ли провести паралельную прямую,
+которая бы делила расстояние между ними пополам?
+"""
 
-# вопрос: есть ли какие-нибудь ограничения?
-
-# Я думаю, что нужно проверить что у нас вопервых есть корректные пары по Y.
-# тоесть, проверить, что у Y1 есть Y2 точка.
-# можно собрать словарь точек Y c суммами всех точек по X
-# перебрать весь словарь, убедившись, что у всех точек Y одинаковая середина
-
-
-# def isReflected(nodes_left, nodes_right) -> bool:
-#     y_dict = {}
-
-#     for x1, y1 in nodes_left:
-#         if y1 in y_dict:
-#             y_dict[y1][0] += x1
-#         else:
-#             y_dict[y1] = [x1, 0]
-
-#     for x1, y1 in nodes_right:
-#         if y1 in y_dict:
-#             y_dict[y1][1] += x1
-#         else:
-#             y_dict[y1] = [0, x1]
-
-#     temp_middle = None
-
-#     for x_list in y_dict.values():
-#         temp_value = (x_list[0] + x_list[1]) / 2
-
-#         if temp_middle is None:
-#             temp_middle = temp_value
-#         if temp_value != temp_middle:
-#             return False
-
-#     return True
-
-
-# я не правильно понял задачу. Дано n точек в рандомный местах, нужно найти есть ли между ними линия
 
 def isReflected(points: list[list[int]]) -> bool:
     max_x, min_x = float("-inf"), float("inf")
@@ -46,16 +11,20 @@ def isReflected(points: list[list[int]]) -> bool:
 
     for x, y in points:
         max_x, min_x = max(max_x, x), min(min_x, x)
-        unique_points.add((x, y,))
-    
+        unique_points.add(
+            (
+                x,
+                y,
+            )
+        )
+
     summ_x = max_x + min_x
 
     for x, y in unique_points:
         if not (summ_x - x, y) in unique_points:
             return False
-    
-    return True
 
+    return True
 
 
 if __name__ == "__main__":
