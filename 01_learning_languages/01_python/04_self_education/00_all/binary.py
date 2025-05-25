689cad18d034f1b0550e8f4b9a2e58b5f0d028ef
DrVeles <frolov.vasilij.2014@yandex.ru>
2025-05-25 22:57:52 +0300

    upd: binary search

--- 01_learning_languages/01_python/04_self_education/00_all/binary.py
+++ 01_learning_languages/01_python/04_self_education/00_all/binary.py
@@ -3,7 +3,7 @@ def binary(array, k):
     right = len(array) - 1
 
     while left <= right:
-        mid = right - left // 2
+        mid = left + (right - left) // 2
 
         if array[mid] == k:
             return mid
@@ -14,3 +14,13 @@ def binary(array, k):
             right = mid - 1
 
     return -1
+
+
+if __name__ == "__main__":
+    assert binary([1, 2, 3, 4, 5], 3) == 2
+    assert binary([], 1) == -1
+    assert binary([1, 2, 4, 5], 3) == -1
+    assert binary([1, 2, 3, 4, 5], -1) == -1
+    assert binary([1, 2, 3], 4) == -1
+    assert binary([0, 1, 2, 3], 0) == 0
+    assert binary((0, 1, 2, 3), 3) == 3
